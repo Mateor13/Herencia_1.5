e26e7f826fe757e1060e911d5ba17e1fb8607ab0
anthony55234 <anthony.astudillo@epn.edu.ec>
2024-05-23 08:51:54 -0500

    SEGUNDO CAMBIO

---
+++ src/Heptagono.java
@@ -0,0 +1,35 @@
+public class Heptagono extends FigurasGeometricas{
+    Double lado, apotema;
+    public Heptagono(){
+        super("", 10);
+    }
+
+    public Heptagono(String nombre, int nLados, Double apotema, Double lado) {
+        super(nombre, nLados);
+        this.lado = lado;
+        this.apotema = apotema;
+    }
+
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+
+    public Double getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(Double apotema) {
+        this.apotema = apotema;
+    }
+    public double calcularArea(){
+        return (nLados*lado*apotema)/2;
+    }
+    public double calcularPerimetro(){
+        return nLados*apotema;
+    }
+}
